32e69fd70021fc20b99d3ae44634e8c5531da30c
Thomas Passelegue <thomas.passelegue@gmail.com>
2017-12-02 14:25:33 -0500

    Déplacement du personnage

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -8,7 +8,8 @@ public class CharacterController : MonoBehaviour
 	private bool toright = true;
 	private bool facedown = true;
 	private Animator character_animator;
-	private Rigidbody2D character_body;	
+	private Rigidbody2D character_body;
+	public int tileSize = 16;
 	
 	// Use this for initialization
 	void Start ()
@@ -19,6 +20,22 @@ public class CharacterController : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			character_body.transform.localPosition = Vector3.MoveTowards(character_body.transform.localPosition, new Vector3(character_body.transform.localPosition.x, character_body.transform.localPosition.y+tileSize, character_body.transform.localPosition.z), 5*Time.deltaTime );
+			//move vers le haut
+		}else if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			character_body.transform.localPosition = Vector3.MoveTowards(character_body.transform.localPosition, new Vector3(character_body.transform.localPosition.x, character_body.transform.localPosition.y-tileSize, character_body.transform.localPosition.z), 5*Time.deltaTime );
+			//move vers le bas
+		}else if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			character_body.transform.localPosition = Vector3.MoveTowards(character_body.transform.localPosition, new Vector3(character_body.transform.localPosition.x+tileSize, character_body.transform.localPosition.y, character_body.transform.localPosition.z), 5*Time.deltaTime );
+			//move vers la droite
+		}else if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			character_body.transform.localPosition = Vector3.MoveTowards(character_body.transform.localPosition, new Vector3(character_body.transform.localPosition.x-tileSize, character_body.transform.localPosition.y, character_body.transform.localPosition.z), 5*Time.deltaTime );
+			//move vers la gauche
+		}
 	}
 }
