4745da7c4292c24270d27c9e1cf21533e6f17634
apomalyn <chretienxaiver42@gmail.com>
2017-12-02 11:11:06 -0500

    Fix bug

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour{
+
+	public BoardManager boardManager;
+
+	private int level = 3;
+
+	private void Awake(){
+		boardManager = GetComponent<BoardManager>();
+		InitGame();
+	}
+
+	private void InitGame(){
+		boardManager.setupScene(level);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
