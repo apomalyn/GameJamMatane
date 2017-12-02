012f300ea7bd8e0e854c76d58bc484529d5eb473
Thomas Passelegue <thomas.passelegue@gmail.com>
2017-12-02 11:29:36 -0500

    Slice des spritesheets (encore) plus petites modifications dans CharacterController

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour {
+public class CharacterController : MonoBehaviour
+{
 
+	private bool toright = true;
+	private bool facedown = true;
+	private Animator character_animator;
+	private Rigidbody2D character_body;	
+	
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		this.character_body = this.GetComponent<Rigidbody2D>();
+		this.character_animator = this.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
