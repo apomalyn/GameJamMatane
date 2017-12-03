408b8652ae5efb1077f1498836e2c0569b736263
Thomas Passelegue <thomas.passelegue@gmail.com>
2017-12-03 10:05:59 -0500

    Menu sexy (V.04)

--- Assets/Scripts/CharacterControllerMainMenu.cs
+++ Assets/Scripts/CharacterControllerMainMenu.cs
@@ -13,6 +13,9 @@ public class CharacterControllerMainMenu : MonoBehaviour
 	private float speed = 5f;
 	public AudioSource attackSound;
 	public AudioClip quack;
+	public GameObject menuLogo;
+	public GameObject menuLogo2;
+	private bool logo;
 	
 	// Use this for initialization
 	void Start ()
@@ -20,67 +23,77 @@ public class CharacterControllerMainMenu : MonoBehaviour
 		this.character_body = this.GetComponent<Rigidbody2D>();
 		this.character_animator = this.GetComponent<Animator>();
 		attackSound = GetComponent<AudioSource>();
+		logo = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 		character_animator.SetBool("Attack", false);
-
+		if (Input.anyKeyDown)
+		{
+			if (logo)
+			{
+				logo = false;
+				Destroy(menuLogo2);
+				Destroy(menuLogo);
+			}
+		}
 		if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.RightArrow))
-		{
-			character_body.transform.Translate(0.005f, 0.005f, 0);
-		}
-		else if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftArrow))
-		{
-			character_body.transform.Translate(-0.005f, 0.005f, 0);
-		}
-		else if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.RightArrow))
-		{
-			character_body.transform.Translate(0.005f, -0.005f, 0);
-		}
-		else if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.LeftArrow))
-		{
-			character_body.transform.Translate(-0.005f, -0.005f, 0);
-		}
-		else if (Input.GetKey(KeyCode.UpArrow))
-		{
-			//move vers le haut
-			character_body.transform.Translate(0, 0.005f, 0);
-		}
-		else if (Input.GetKey(KeyCode.DownArrow))
-		{
-			character_body.transform.Translate(0, -0.005f, 0);
-			//move vers le bas
-		}
-		else if (Input.GetKey(KeyCode.RightArrow))
-		{
-			if (!toright)
 			{
-				flip();
+				character_body.transform.Translate(0.005f, 0.005f, 0);
 			}
-			character_body.transform.Translate(0.005f, 0, 0);
-			//move vers la droite
-		}
-		else if (Input.GetKey(KeyCode.LeftArrow))
-		{
-			if (toright)
+			else if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftArrow))
 			{
-				flip();
+				character_body.transform.Translate(-0.005f, 0.005f, 0);
 			}
-			character_body.transform.Translate(-0.005f, 0, 0);
-			//move vers la gauche
-		}
-		else if (Input.GetKeyDown(KeyCode.Space))
-		{
-			//attack
-			//Attack sound
-			attackSound.PlayOneShot(quack, 0.3f);
-			//Attack animation
-			character_animator.SetBool("Attack", true);
+			else if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.RightArrow))
+			{
+				character_body.transform.Translate(0.005f, -0.005f, 0);
+			}
+			else if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.LeftArrow))
+			{
+				character_body.transform.Translate(-0.005f, -0.005f, 0);
+			}
+			else if (Input.GetKey(KeyCode.UpArrow))
+			{
+				//move vers le haut
+				character_body.transform.Translate(0, 0.005f, 0);
+			}
+			else if (Input.GetKey(KeyCode.DownArrow))
+			{
+				character_body.transform.Translate(0, -0.005f, 0);
+				//move vers le bas
+			}
+			else if (Input.GetKey(KeyCode.RightArrow))
+			{
+				if (!toright)
+				{
+					flip();
+				}
+				character_body.transform.Translate(0.005f, 0, 0);
+				//move vers la droite
+			}
+			else if (Input.GetKey(KeyCode.LeftArrow))
+			{
+				if (toright)
+				{
+					flip();
+				}
+				character_body.transform.Translate(-0.005f, 0, 0);
+				//move vers la gauche
+			}
+			else if (Input.GetKeyDown(KeyCode.Space))
+			{
+				//attack
+				//Attack sound
+				attackSound.PlayOneShot(quack, 0.3f);
+				//Attack animation
+				character_animator.SetBool("Attack", true);
 
 
-		}
+			}
+		
 	}
 
 	void flip()
