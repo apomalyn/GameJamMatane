affff50345a95e754b9d61141be658ce6bc316fc
LaurieFer <laurie.fernanli@gmail.com>
2017-12-03 20:38:33 +0100

    Deplacement des ennemies

--- Assets/Scripts/EnnemyController.cs
+++ Assets/Scripts/EnnemyController.cs
@@ -7,9 +7,9 @@ using UnityEngine;
 public class EnnemyController : MonoBehaviour
 {
 	public enum EnemyType{
-		Bat,
-		Skeleton,
-		BlackMage
+		Bat = 0,
+		Skeleton = 1,
+		BlackMage = 2
 	}
 
 	public enum Direction{
@@ -25,7 +25,11 @@ public class EnnemyController : MonoBehaviour
 	
 	private int life;
 	private Rigidbody2D ennemy_body;
+	private int restMove = 0;
+	private int indexCurrentMove = 0;
 
+	public bool go = true;
+	
 	public EnemyType type;
 	
 	// Pattern de mouvement
@@ -34,6 +38,8 @@ public class EnnemyController : MonoBehaviour
 		{4, 4, 0, 0},
 		{4, 4, 0, 0}
 	};
+
+	private const int MAX_MOVE = 4;
 
 	private Direction[,] directionPattern = {
 		{Direction.Up, Direction.Left, Direction.Up, Direction.Right},
@@ -52,7 +58,17 @@ public class EnnemyController : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		deplacement();
+		
+		if (go)
+		{
+			deplacement();
+			go = false;
+		}
+		else
+		{
+			System.Threading.Thread.Sleep(2000);
+			go = true;
+		}
 	}
 	
 	void OnTriggerEnter2D(Collider2D collider)
@@ -69,8 +85,41 @@ public class EnnemyController : MonoBehaviour
 
 	void deplacement()
 	{
-		
-	}
+		Vector3 position = new Vector3();
+
+		if (restMove == 0)
+		{
+			indexCurrentMove = (indexCurrentMove + 1 < MAX_MOVE) ? indexCurrentMove + 1 : 0;
+			while (pattern[(int) type, indexCurrentMove] == 0){
+				indexCurrentMove = (indexCurrentMove + 1 < MAX_MOVE) ? indexCurrentMove + 1 : 0;
+			}
+			restMove = pattern[(int) type, indexCurrentMove];
+		}
+
+		switch (directionPattern[(int) type, indexCurrentMove])
+		{
+			case Direction.Up:
+				//Translate
+				position = new Vector3(0, 0.1600f, 0);
+				break;
+			case Direction.Down:
+				//Translate
+				position = new Vector3(0, -0.1600f, 0);
+				break;
+			case Direction.Left:
+
+				//Translate
+				position = new Vector3(-0.1600f, 0, 0);
+				break;
+			case Direction.Right:
+				position = new Vector3(0.1600f, 0, 0);
+				break;
+		}
+		if (position != new Vector3()){
+			ennemy_body.transform.Translate(position, Space.World);
+			restMove--;
+		}
+}
 
 	void destroy()
 	{
